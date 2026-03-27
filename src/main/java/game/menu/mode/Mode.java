52f4bd90aa982e48b458ab79c755abe003f0f863
AumJi <studiedbyaumji@gmail.com>
2026-03-28 01:40:23 +0700

    fix: retexture

--- src/main/java/game/menu/mode/Mode.java
+++ src/main/java/game/menu/mode/Mode.java
@@ -13,7 +13,14 @@ import game.menu.components.UIButton;
 public class Mode extends Scene {
     private FontAtlas font;
     private Texture btnTexture;
-    private Texture cardTexture;
+    private Texture backgroundTexture;
+    private Texture backBtnTexture;
+    private Texture stageButtonTexture;
+    private Texture overrunButtonTexture;
+    private Texture upgradeButtonTexture;
+    private Texture shopButtonTexture;
+    private Texture stageCardTexture;
+    private Texture overrunCardTexture;
 
     private UIButton stageBtn;
     private UIButton overrunBtn;
@@ -23,41 +30,47 @@ public class Mode extends Scene {
 
     @Override
     public void init(int width, int height) {
+        backgroundTexture = new Texture("textures/bg.png");
         font = new FontAtlas("GeistMono-Regular.otf", 32);
-        btnTexture = new Texture("textures/button_test.png");
-        cardTexture = new Texture("textures/solid.png"); // placeholder for card
+        backBtnTexture = new Texture("textures/btn_back.png");
+        stageButtonTexture = new Texture("textures/mode/btn_stage.png");
+        overrunButtonTexture = new Texture("textures/mode/btn_overrun.png");
+        upgradeButtonTexture = new Texture("textures/mode/btn_upgrade.png");
+        shopButtonTexture = new Texture("textures/mode/btn_shop.png");
+        stageCardTexture = new Texture("textures/mode/stage.png");
+        overrunCardTexture = new Texture("textures/mode/overrun.png");
 
-        Vec2 btnSize = new Vec2(300, 100);
+        Vec2 btnSize = new Vec2(256, 92);
 
         stageBtn = new UIButton(
                 super.layout.center(-200, 150),
                 btnSize,
-                "Stage",
-                btnTexture);
+                "",
+                stageButtonTexture);
 
         overrunBtn = new UIButton(
                 super.layout.center(200, 150),
                 btnSize,
-                "Overrun",
-                btnTexture);
+                "",
+                overrunButtonTexture);
 
         upgradeBtn = new UIButton(
                 super.layout.bottomRight(400, 50),
                 new Vec2(150, 50),
-                "Upgrade",
-                btnTexture);
+                "",
+                upgradeButtonTexture);
 
         shopBtn = new UIButton(
                 super.layout.bottomRight(200, 50),
                 new Vec2(150, 50),
-                "Shop",
-                btnTexture);
+                "",
+                shopButtonTexture);
 
         backBtn = new UIButton(
                 super.layout.topLeft(100, 50),
                 new Vec2(100, 50),
-                "Back",
-                btnTexture);
+                "",
+                backBtnTexture);
 
         super.uiManager.add(stageBtn);
         super.uiManager.add(overrunBtn);
@@ -94,6 +107,11 @@ public class Mode extends Scene {
     public void renderUI(float delta) {
         glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
 
+        Vec2 backgroundPos = super.layout.center(0, 0);
+        Vec2 backgroundSize = new Vec2(super.layout.res.x, super.layout.res.y);
+    
+        super.batch.draw(backgroundTexture, backgroundPos.x, backgroundPos.y, backgroundSize.x, backgroundSize.y);
+
         // Coins Display
         Vec2 coinPos = super.layout.topRight(150, 50);
         font.drawTextAligned(super.batch, "Coins: " + game.data.PlayerData.coins, coinPos.x, coinPos.y,
@@ -101,11 +119,11 @@ public class Mode extends Scene {
 
         // draw dummy cards
         super.batch.setColor(new Color(0.8f, 0.8f, 0.8f, 1.0f));
-        Vec2 stageCardPos = super.layout.center(-200, -100);
-        super.batch.draw(cardTexture, stageCardPos.x, stageCardPos.y, 300, 300);
+        Vec2 stageCardPos = super.layout.center(-200, -50);
+        super.batch.draw(stageCardTexture, stageCardPos.x, stageCardPos.y, 300 * 1.25f, 300 * 1.25f);
 
-        Vec2 overrunCardPos = super.layout.center(200, -100);
-        super.batch.draw(cardTexture, overrunCardPos.x, overrunCardPos.y, 300, 300);
+        Vec2 overrunCardPos = super.layout.center(200, -50);
+        super.batch.draw(overrunCardTexture, overrunCardPos.x, overrunCardPos.y, 300 * 1.25f, 300 * 1.25f);
 
         super.uiManager.render(super.batch, font, mouseScreen);
     }
@@ -114,9 +132,21 @@ public class Mode extends Scene {
     public void cleanup() {
         if (font != null)
             font.cleanup();
+        if (backgroundTexture != null)
+            backgroundTexture.cleanup();
         if (btnTexture != null)
             btnTexture.cleanup();
-        if (cardTexture != null)
-            cardTexture.cleanup();
+        if (stageButtonTexture != null)
+            stageButtonTexture.cleanup();
+        if (overrunButtonTexture != null)
+            overrunButtonTexture.cleanup();
+        if (upgradeButtonTexture != null)
+            upgradeButtonTexture.cleanup();
+        if (shopButtonTexture != null)
+            shopButtonTexture.cleanup();
+        if (stageCardTexture != null)
+            stageCardTexture.cleanup();
+        if (overrunCardTexture != null)
+            overrunCardTexture.cleanup();
     }
 }
